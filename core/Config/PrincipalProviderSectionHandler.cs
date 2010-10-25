fa98dcae052130d6c88ee8c4524606192c6c58aa
vincent.valenlee <vincent.valenlee@dd3c6ad2-0546-0410-9562-71b263f4db86>
2010-10-25 14:23:34 +0000

    加入DBPrincipalProviders基于数据库的身份提供者

--- core/Config/PrincipalProviderSectionHandler.cs
+++ core/Config/PrincipalProviderSectionHandler.cs
@@ -11,33 +11,55 @@ namespace CrystalWall.Config
     /// <summary>
     /// 用于在xml配置文件中解析principal提供者的处理器
     /// </summary>
-    public class PrincipalProviderSectionHandler : IConfigurationSectionHandler 
+    public class PrincipalProviderSectionHandler : IConfigurationSectionHandler
     {
         public object Create(object parent, object configContext, System.Xml.XmlNode section)
         {
-              try
-              {
-                  IPrincipalProvider provider;
-                  if (section.Attributes["assembly"] == null)
-                  {
-                      //没有指定程序集，但可能在class中指定全限定名。首先使用当前的程序集加载，否则使用全限定名加载
-                      provider = (IPrincipalProvider)Assembly.GetExecutingAssembly().CreateInstance(section.Attributes["class"].Name);
-                      if (provider == null)
-                          provider = (IPrincipalProvider)Type.GetType(section.Attributes["class"].Name, true).GetConstructor(new Type[0]).Invoke(new object[0]);
-                  }
-                  else
-                  {
-                      provider = (IPrincipalProvider)Assembly.LoadFrom(section.Attributes["assembly"].Name).CreateInstance(section.Attributes["class"].Name);
-                  }
-                  //初始化provider数据（将根据具体的provider并根据xml中的配置进行初始化，由子类决定）
-                  provider.InitData(section, null, null);
-                  return provider;
-              }
-              catch (Exception e)
-              {
-                  ServiceManager.LoggingService.Error("无法加载指定的身份提供者，请检查配置文件是否正确", e);
-                  throw e;
-              }
+            if (section.Attributes["class"] == null)
+                throw new ConfigurationException(section.Name, "身份提供者配置节点中必须具有class属性指定提供者类型");
+            IPrincipalProvider provider = null;
+            try
+            {
+                if (section.Attributes["assembly"] == null)
+                {
+                    //没有指定程序集，但可能在class中指定全限定名。首先使用当前运行的程序集加载，然后使用crystalwall程序集加载，最后使用全限定名加载
+                    try
+                    {
+                        provider = (IPrincipalProvider)Assembly.GetExecutingAssembly().CreateInstance(section.Attributes["class"].Value);
+                        if (provider != null)
+                            goto init;
+                    }
+                    catch (Exception e)
+                    {
+                        ServiceManager.LoggingService.Error("当前运行的程序集中无法加载提供者", e);
+                    }
+                    try
+                    {
+                        provider = (IPrincipalProvider)Assembly.GetAssembly(typeof(IPrincipalProvider)).CreateInstance(section.Attributes["class"].Value);
+                        if (provider != null)
+                            goto init;
+                    }
+                    catch (Exception e)
+                    {
+                        ServiceManager.LoggingService.Error("指定的提供者" + section.Attributes["class"].Value + "不是默认的提供者，将使用全限定名加载", e);
+                    }
+                    provider = (IPrincipalProvider)Type.GetType(section.Attributes["class"].Value, true).GetConstructor(new Type[0]).Invoke(new object[0]);
+                }
+                else
+                {
+                    provider = (IPrincipalProvider)Assembly.LoadFrom(section.Attributes["assembly"].Value).CreateInstance(section.Attributes["class"].Value);
+                }
+                if (provider == null)
+                    throw new ConfigurationException(section.Name, "身份提供者配置节点中指定提供者类型无法加载");
+            }
+            catch (Exception e)
+            {
+                ServiceManager.LoggingService.Error("无法加载指定的身份提供者，请检查配置文件是否正确", e);
+                throw e;
+            }
+        //初始化provider数据（将根据具体的provider并根据xml中的配置进行初始化，由子类决定）
+        init: provider.InitData(section, null, null);
+            return provider;
         }
     }
 }
