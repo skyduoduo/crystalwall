bdd38cd713bf12848cac4a1e46edc627ebbdb4c0
vincent.valenlee <vincent.valenlee@dd3c6ad2-0546-0410-9562-71b263f4db86>
2010-10-28 13:53:26 +0000

    1、添加ConfigurationFile类用于读取配置文件并获取处理section 2、身份提供者处理器测试通过

---
+++ core/Config/ConfigurationFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Xml;
+
+namespace CrystalWall.Config
+{
+    public class ConfigurationFile
+    {
+        private Configuration configuration;
+
+        public ConfigurationFile(string path)
+        {
+            configuration = ConfigurationManager.OpenExeConfiguration(path);
+        }
+
+        public object GetSection(string sectionPath, IConfigurationSectionHandler handler)
+        {
+            string rowXml = configuration.GetSection(sectionPath).SectionInformation.GetRawXml();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(rowXml);
+            XmlNode node = doc.DocumentElement;
+            return handler.Create(null, null, node);
+        }
+    }
+}
